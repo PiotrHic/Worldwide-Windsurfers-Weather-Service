76b238970c06a53ab6824cb5561d22caba6cccff
Piotr Zbigniew Hic <piotrhic@gmail.com>
2026-02-20 20:52:50 +0100

    Initial Project add

---
+++ src/main/java/org/example/weatherforecastapiapp/WeatherForecastApiAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.weatherforecastapiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherForecastApiAppApplication {
+
+    public static void main ( String[] args ) {
+        SpringApplication.run ( WeatherForecastApiAppApplication.class, args );
+    }
+
+}
