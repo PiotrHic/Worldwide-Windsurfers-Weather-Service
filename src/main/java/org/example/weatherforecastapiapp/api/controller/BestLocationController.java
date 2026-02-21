5c298a90609d391c971878af4603599221d839b7
Piotr Zbigniew Hic <piotrhic@gmail.com>
2026-02-21 11:59:45 +0100

    Create Controller

--- src/main/java/org/example/weatherforecastapiapp/api/controller/BestLocationController.java
+++ src/main/java/org/example/weatherforecastapiapp/api/controller/BestLocationController.java
@@ -13,7 +13,6 @@ import java.time.format.DateTimeParseException;
 public class BestLocationController {
 
     private final LocationEvaluationServiceImpl locationEvaluationService;
-    private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public BestLocationController(LocationEvaluationServiceImpl locationEvaluationService) {
         this.locationEvaluationService = locationEvaluationService;
@@ -21,24 +20,6 @@ public class BestLocationController {
 
     @GetMapping("/best-location")
     public BestLocationResponse getBestLocation(@RequestParam String date) {
-
-        try {
-            LocalDate.parse(date, dateFormatter);
-        } catch (DateTimeParseException e) {
-            throw new InvalidDateException("Invalid date format. Expected yyyy-MM-dd.");
-        }
-
-        BestLocationResponse response = locationEvaluationService.evaluateBestLocation(date);
-
-        if (response.message() != null && response.location() == null) {
-            String msg = response.message();
-            if (msg.contains("Invalid date format")) {
-                throw new InvalidDateException(msg);
-            } else if (msg.contains("Date must be within")) {
-                throw new DateOutOfRangeException(msg);
-            }
-        }
-
-        return response;
+        return locationEvaluationService.evaluateBestLocation(date);
     }
 }
