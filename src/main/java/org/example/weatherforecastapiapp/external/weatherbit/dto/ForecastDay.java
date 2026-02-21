35adea98d9068f4251038af32e7adb732c31d04d
Piotr Zbigniew Hic <piotrhic@gmail.com>
2026-02-21 08:44:43 +0100

    ForecastDay external DTO refractor v1

--- src/main/java/org/example/weatherforecastapiapp/external/weatherbit/dto/ForecastDay.java
+++ src/main/java/org/example/weatherforecastapiapp/external/weatherbit/dto/ForecastDay.java
@@ -1,9 +1,15 @@
 package org.example.weatherforecastapiapp.external.weatherbit.dto;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public record ForecastDay(
         LocalDate date,
         double temperature,
         double windSpeed
-) {}
+) {
+
+    public ForecastDay {
+        Objects.requireNonNull(date, "date cannot be null");
+    }
+}
