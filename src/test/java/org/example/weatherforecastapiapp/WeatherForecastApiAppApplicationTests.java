5ecff8fa5f9b78e4f2cf535e5955bcfcbcc2139a
Piotr Zbigniew Hic <piotrhic@gmail.com>
2026-02-21 19:17:50 +0100

    Fix WeatherForecastApiAppApplicationTests issue

--- src/test/java/org/example/weatherforecastapiapp/WeatherForecastApiAppApplicationTests.java
+++ src/test/java/org/example/weatherforecastapiapp/WeatherForecastApiAppApplicationTests.java
@@ -2,8 +2,10 @@ package org.example.weatherforecastapiapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "weatherbit.api-key=dummy-key-for-tests")
 class WeatherForecastApiAppApplicationTests {
 
     @Test
